1440571b89f8fdf3b7e62887496b81b4b50dd2d1
Aldiyar <nesquikyey@gmail.com>
2025-04-22 16:01:28 +0500

    make changes in api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
 const API_BASE = import.meta.env.VITE_API_BASE;
 
 const API = {
-    ALL_PRODUCTS: `${API_BASE}/products`,
+    ALL_PRODUCTS: (limit?: number, skip?: number) => `${API_BASE}/products?limit=${limit || 10}&skip=${skip || 0}`,
+    SINGLE_PRODUCTS: (id: number) => `${API_BASE}/${id}`,
     SEARCH_PRODUCTS: (query: string) => `${API_BASE}/products/search?q=${query}`,
     PRODUCTS_CATEGORIES: `${API_BASE}/products/categories`,
-    PRODUCTS_CATEGORIES_LIST: `${API_BASE}/products/category-list`,
     CATEGORY_PRODUCTS: (categoryName: string) => `${API_BASE}/products/category/${categoryName}`,
 }
 
