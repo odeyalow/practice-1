2172c71d883a88f51b038a207fa775d0ae2e2f51
Aldiyar <nesquikyey@gmail.com>
2025-05-05 23:42:01 +0500

    fix search results page rendering every search query change

--- src/shared/components/searchResultsPage.tsx
+++ src/shared/components/searchResultsPage.tsx
@@ -6,16 +6,21 @@ import { RootState } from '@/app/store';
 import useGetData from '@/shared/hooks/useGetData';
 import { IProductResponse } from '@/entities/IProduct';
 import API from '@/api';
+import { useMemo } from 'react';
+import { useParams } from 'react-router';
 
 const SearchResultsPage = () => {
     const searchQuery = useSelector((state: RootState) => state.search.searchQuery);
     const { data } = useGetData<IProductResponse>(API.SEARCH_PRODUCTS(searchQuery), 'searchResult', searchQuery, true);
+    const { query } = useParams();
 
-    return (
-        data && <ContentWrapper title={`Search results for "${searchQuery}"`}>
-            <ProductsCollection placement="grid" products={data?.products}/>
-        </ContentWrapper>
-    );
+    const render = useMemo(() => {
+        return data &&  <ContentWrapper title={`Search results for "${searchQuery}"`}>
+                        <ProductsCollection placement="grid" products={data?.products}/>
+                    </ContentWrapper>
+    }, [query])
+
+    return render;
 }
  
 export default SearchResultsPage;
